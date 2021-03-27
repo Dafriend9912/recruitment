4a5afe67fc8d74bed4103b44d02e414d55f3effc
Drew Friend <dafriend@knox.edu>
2021-03-27 15:08:39 -0500

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="https://avatars.githubusercontent.com/u/43389539?v=4">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
