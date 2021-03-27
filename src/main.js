db0857e25345de2e9e5811184b46164a49395ae7
Drew Friend <dafriend@knox.edu>
2021-03-27 15:18:49 -0500

    Commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const app = Vue.createApp({
+  '<h1>Hello World</h1>',
+})
+app.mount('#app')
