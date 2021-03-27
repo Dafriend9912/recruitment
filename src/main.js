438d0abcc835ee52de2b8d72bcb72f624d8a0d7d
Drew Friend <dafriend@knox.edu>
2021-03-27 15:28:49 -0500

    Commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 
 const app = Vue.createApp({
-  template: '<h1>Hello World</h1>'
+  template: '<h1>Hello World</h1>',
+  data() {
+    return {
+      picture: 'https://avatars.githubusercontent.com/u/43389539?s=48&v=4'
+    }
+  }
 })
 app.mount('#app')
